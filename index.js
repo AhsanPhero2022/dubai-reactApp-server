7359f4db7d62edd3805b6ec8ff683552866b0711
AhsanPhero2022 <whitehat2090@gmail.com>
2024-01-24 03:57:06 -0800

    user get done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+require("dotenv").config();
 const { MongoClient, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,7 +12,7 @@ app.use(express.json());
 // dubai-jobTask
 // v0bn3GMrv9IypXus
 
-const uri = `mongodb+srv://dubai-jobTask:v0bn3GMrv9IypXus@cluster0.xsh8x1y.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xsh8x1y.mongodb.net/?retryWrites=true&w=majority`;
 // Create a new MongoClient
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -25,20 +25,15 @@ async function run() {
 
     const userCollection = client.db("user").collection("frozen");
 
-    app.get("/frozen", async (req, res) => {
-      const cursor = userCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-
-  
-
     app.post("/user", async (req, res) => {
       const newUser = req.body;
-
       const result = await userCollection.insertOne(newUser);
       res.send(result);
     });
+    app.get("/user" , async (req, res)=>{
+        const result = await userCollection.find().toArray();
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -53,9 +48,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Assignment server is Running");
+  res.send("Jod-Task server is Running");
 });
 
 app.listen(port, () => {
-  console.log(`Assignment Server Is Running on Port ${port}`);
+  console.log(`Jod-Task Server Is Running on Port ${port}`);
 });
